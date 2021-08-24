05368710d2f80a6f09e351c2e1f0c17d16e7f922
프로젝트 공간 <mu_2375@naver.com>
2021-08-24 23:45:55 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Automate-enrollment-for-DCU
+ DCU 수강신청 자동화 스크립트
---
+++ myrefect.py
@@ -0,0 +1,159 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import configparser
+
+def load_information():
+    con_parser = configparser.ConfigParser()
+    con_parser.read('config.ini')
+
+    config = con_parser['config']
+    user_id = config["id"]
+    user_pw = config["pw"]
+
+    return user_id, user_pw
+
+def inject_captcha_bypass():
+    driver.execute_script('''
+        function myHookCaptcha(str1, str2) {	
+            var canvas = frm.document.getElementById("chapcha");
+            if(!!canvas){
+                var ctx = frm.document.getElementById("chapcha").getContext("2d");
+                ctx.clearRect(0,0,32,20); 
+                ctx.beginPath();
+                ctx.fillText(str1, 16, 15);
+                //frm.document.getElementById("addrndKey").value = str1;
+                frm.document.getElementById("cartrndKey").value = str1;
+            }
+        }
+        showCaptchaString = myHookCaptcha;
+    ''')
+
+def try_login(user_id,user_pw):
+    login_frame = driver.find_element_by_id("ifrm")
+    driver.switch_to.frame(login_frame)
+
+    id = driver.find_element_by_css_selector("#login_id")
+    id.send_keys(user_id)
+
+    pw = driver.find_element_by_css_selector("#password")
+    pw.send_keys(user_pw)
+
+    login_btn = driver.find_element_by_css_selector("#btn_login")
+    login_btn.click()
+
+    driver.switch_to.default_content()
+
+def try_request():
+    in_frame = driver.find_element_by_id("ifrm")
+    driver.switch_to.frame(in_frame)
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#cartInfo .table_view_tr1")))
+
+    driver.execute_script('''
+        function my_cart_submit(){
+
+            // key값 입력여부 확인.
+            var cartrndKey = document.getElementById("cartrndKey").value;
+
+            if(cartrndKey.length != 2){
+                document.getElementById("cartrndKey").focus();
+                alert("수강꾸러미 일괄신청 KEY를 입력하세요.");
+                return false;
+            }
+
+            // 자료 선택여부 확인
+            var bCheckYn = false;
+            for(var i=0; i < document.getElementById("dummy3").elements.length; i++){
+                if(document.getElementById("dummy3").elements[i].type == "checkbox"){
+                    e = document.getElementById("dummy3").elements[i];
+                    if(e.checked){
+                        bCheckYn = true;
+                        continue;
+                    }
+                }
+            }
+
+            if(!bCheckYn){
+                alert("수강꾸러미 일괄신청 자료를 선택해주세요.");	
+                return false;
+            }
+
+            actionPro("S");
+            cartMsgClear();
+
+            // 수강신청 처리
+            var vR = "||";
+            var vArr;
+
+            for(var i=0; i < document.getElementById("dummy3").elements.length; i++){
+                if(document.getElementById("dummy3").elements[i].type == "checkbox"){
+                    e = document.getElementById("dummy3").elements[i];
+                    if(e.checked){
+                        vArr = e.value.split(vR);
+                        parent.client_mesg('ADD_CLIST' + strTok + vArr[0] + strTok + vArr[1] + strTok + cartrndKey);
+                        vSleep(1000);		// 1초 후에 날림.
+                    }
+                }
+            }
+
+            // chapchar 변경
+            parent.client_mesg("PONG");
+
+            document.getElementById("cartrndKey").value="";
+            document.getElementById("addrndKey").value="";
+            return false;
+        }
+        cart_submit = my_cart_submit
+    ''')
+    driver.execute_script("cart_submit();")
+
+#######################################################################################################################
+chromedriver = './chromedriver.exe'
+driver = webdriver.Chrome(chromedriver)
+driver.get('http://sugang.cu.ac.kr/sugang/')
+driver.implicitly_wait(5)
+
+inject_captcha_bypass()
+id, pw = load_information()
+try_login(id,pw)
+try_request()
+
+
+
+
+
+
+
+
+
+
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+        
+    
+
+
